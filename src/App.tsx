7671c78c54de6eb569d87a67fe07ab84a5cbfc04
Alexs Ruiz <ralexs.acu@gmail.com>
2023-06-17 19:08:57 -0600

    feat(App): define main view
    - this menu allow the user navigate to each module

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import CardWrapper from './components/shared/CardWrapper';
+import ButtonComponent from './components/shared/ButtonComponent';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <CardWrapper isCenter heightCard={400} widthCard={400}>
-      <Typography>This is a Test</Typography>
+      <Box p={2} display="flex" flexWrap="wrap" justifyContent="center">
+        <ButtonComponent text="Area" onClickFn={() => navigate('/areas')} />
+        <ButtonComponent
+          text="Tickets"
+          onClickFn={() => navigate('/tickets')}
+        />
+        <ButtonComponent text="Users" onClickFn={() => navigate('/users')} />
+        <ButtonComponent text="Brand" onClickFn={() => navigate('/brands')} />
+      </Box>
     </CardWrapper>
   );
 }
