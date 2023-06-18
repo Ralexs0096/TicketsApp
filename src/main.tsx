e073865cbaccd7fbb3d3afd72f75199b82b1ce0e
Alexs Ruiz <ralexs.acu@gmail.com>
2023-06-17 18:38:07 -0600

    feat(main): add cssBaseline to reset style inconsistencies across browsers and devices

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { routes } from './router/routes';
 import { ThemeProvider } from '@emotion/react';
 import theme from './theme';
+import { CssBaseline } from '@mui/material';
 
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
