9bae8b9dfd3d9ab80c19994fdb21cc664ec1bac2
Alexs Ruiz <ralexs.acu@gmail.com>
2023-09-18 23:15:48 -0600

    test(App): use the new "render" method for feature tour testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,21 @@
-import { screen, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import App from './App';
+import render from './router/testRenderer';
 
-it('displays a +1 button', () => {
+it('App feature tour', async () => {
   render(<App />);
 
-  expect(
-    screen.getByRole('button', {
-      name: /\+1/i
-    })
-  ).toBeInTheDocument();
+  // now we expect to see all button options
+  screen.getByRole('button', {
+    name: /area/i
+  });
+  screen.getByRole('button', {
+    name: /tickets/i
+  });
+  screen.getByRole('button', {
+    name: /brand/i
+  });
+  screen.getByRole('button', {
+    name: /users/i
+  });
 });
