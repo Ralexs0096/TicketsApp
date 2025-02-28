d888e85b38cb87699728bad8cf4ddb490c91de3b
Alexs Ruiz <ralexs.acu@gmail.com>
2025-02-27 23:02:07 -0600

    Set react-router vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 /// <reference types="vitest" />
+import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setup.ts'
-  }
+  plugins: [reactRouter()]
+  // test: {
+  //   include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+  //   globals: true,
+  //   environment: 'jsdom',
+  //   setupFiles: './src/setup.ts'
+  // }
 });
