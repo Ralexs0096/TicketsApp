41fb01e6daa2ecf5cc99f4aa7925cfaff131a6c8
Alex Ruiz <ralexs.acu@gmail.com>
2025-11-09 11:50:19 -0600

    feat(vite.config): add tanstackRouter plugin
    - disable test options temporally

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setup.ts'
-  }
+  plugins: [
+     tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react()
+  ],
+  // test: {
+  //   include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+  //   globals: true,
+  //   environment: 'jsdom',
+  //   setupFiles: './src/setup.ts'
+  // }
 });
