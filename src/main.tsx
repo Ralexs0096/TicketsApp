9345e2dce73371694fa39aa09689849d6a6fa12b
Alex Ruiz <ralexs.acu@gmail.com>
2025-11-09 11:49:46 -0600

    fix(main): remove react-router imports and providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { routes } from './router/routes';
 import theme from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-
-const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );
