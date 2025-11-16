08beba6eb884760d9a74d1f566ce0f328c8f598f
Alex Ruiz <ralexs.acu@gmail.com>
2025-11-16 16:58:51 -0600

    fix(AppBar): remove react router logic and imports

--- src/components/AppBarComponent.tsx
+++ src/components/AppBarComponent.tsx
@@ -1,36 +1,30 @@
 import { AccountCircle } from '@mui/icons-material';
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { AppBar, IconButton, Toolbar } from '@mui/material';
-import { useLocation, useNavigate } from 'react-router-dom';
 
 const AppBarComponent = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-
   return (
-    <AppBar position="static" color="default">
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div>
-          {location.pathname !== '/' && (
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={() => navigate(-1)}
-              color="inherit"
-            >
-              <ArrowBackIosNewIcon />
-            </IconButton>
-          )}
-        </div>
-
+    <AppBar
+      position="static"
+      color="default"
+      sx={{
+        backgroundColor: 'transparent',
+        border: 1,
+        borderRadius: 2,
+        borderColor: 'gray',
+        boxShadow: 'none'
+      }}
+    >
+      <Toolbar
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between'
+        }}
+      >
         <IconButton
           size="large"
           aria-label="account of current user"
           aria-controls="menu-appbar"
           aria-haspopup="true"
-          onClick={() => console.log('')}
           color="inherit"
         >
           <AccountCircle />
