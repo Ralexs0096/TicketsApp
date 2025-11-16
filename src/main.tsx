c9135000868202273caa596020a486985b2b8bdb
Alex Ruiz <ralexs.acu@gmail.com>
2025-11-16 16:57:27 -0600

    feat(app): remove react-router and replace with tanstack router
    - create the initial layout and component entry point

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 
 import theme from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+
+import { routeTree } from './routeTree.gen';
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );
