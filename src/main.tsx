2a7f6e1cf8d3cb97cbae42e28a42c03bea73cb7c
Alex Ruiz <ralexs.acu@gmail.com>
2025-11-28 17:50:20 -0600

    feat(main): add tanstack query devtools config for browser extension

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import theme from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { RouterProvider } from '@tanstack/react-router';
+import theme from './theme';
+import { queryClient, router } from './router';
+import { AuthProvider } from './providers/auth';
+import { QueryClientProvider } from '@tanstack/react-query';
 
-import { routeTree } from './routeTree.gen';
-
-const router = createRouter({ routeTree });
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router;
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__: import('@tanstack/query-core').QueryClient;
   }
 }
+
+// this enable tanstack query devtools using the browser extension
+window.__TANSTACK_QUERY_CLIENT__ = queryClient;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
