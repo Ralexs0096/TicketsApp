8d75d527f5b4cd9839822b8039440debe7e6a22f
Alex Ruiz <ralexs.acu@gmail.com>
2025-12-01 18:13:46 -0600

    feat(auth): define auth session query options

---
+++ src/queries/auth.ts
@@ -0,0 +1,19 @@
+import { queryOptions } from '@tanstack/react-query';
+import { apiClient } from '../apiClient';
+
+export const authQueryOptions = queryOptions({
+  queryKey: ['auth', 'session'],
+  queryFn: async () => {
+    try {
+      return await apiClient.user.fetchUser({
+        userId: 'me'
+      });
+    } catch {
+      return {
+        user: null
+      };
+    }
+  },
+  staleTime: Infinity,
+  gcTime: Infinity
+});
