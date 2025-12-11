bee67939b908fd0a964601928cd0e475638b8fd3
Alex Ruiz <ralexs.acu@gmail.com>
2025-12-10 22:34:33 -0600

    feat(wip): add initial structure of tickets section

--- src/routes/_authenticated/index.tsx
+++ src/routes/_authenticated/index.tsx
@@ -1,15 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
-import CardWrapper from '../../components/shared/CardWrapper';
-import { Box } from '@mui/material';
+import Ticket from '../../modules/tickets/Ticket';
 
 export const Route = createFileRoute('/_authenticated/')({
-  component: App
+  component: Ticket
 });
-
-function App() {
-  return (
-    <CardWrapper isCenter heightCard={400} widthCard={400}>
-      <Box p={2} display="flex" flexWrap="wrap" justifyContent="center"></Box>
-    </CardWrapper>
-  );
-}
