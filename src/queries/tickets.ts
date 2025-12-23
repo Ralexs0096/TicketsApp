644f4cd510d941ad4df09b0ca15ba5cbe0aa966b
Alex Ruiz <ralexs.acu@gmail.com>
2025-12-22 21:12:46 -0600

    feat(tickets/query): add new query to get the recent tickets

---
+++ src/queries/tickets.ts
@@ -0,0 +1,18 @@
+import { queryOptions } from '@tanstack/react-query';
+import { apiClient } from '../apiClient';
+
+export function getAllTickets() {
+  return queryOptions({
+    queryKey: ['ticket', 'all'],
+    queryFn: async () => {
+      try {
+        const result = await apiClient.ticket.fetchAllTickets();
+        return result;
+      } catch {
+        throw new Error('Error fetching tickets');
+      }
+    },
+    staleTime: Infinity,
+    gcTime: Infinity
+  });
+}
