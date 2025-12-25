110003cbccabe19e0afa52872e58c851e8a73d8f
Alex Ruiz <ralexs.acu@gmail.com>
2025-12-25 17:41:47 -0600

    feat(TicketList): call getAllTickets query to populate the ticket list

--- src/modules/tickets/components/TicketsList.tsx
+++ src/modules/tickets/components/TicketsList.tsx
@@ -1,12 +1,20 @@
-import { Box } from '@mui/material';
+import { Box, CircularProgress } from '@mui/material';
 import TicketCard from './TicketCard';
+import { useQuery } from '@tanstack/react-query';
+import { getAllTickets } from '../../../queries/tickets';
 
 const TicketsList = () => {
+  const { data, isFetching } = useQuery(getAllTickets());
+
+  if (isFetching) {
+    return <CircularProgress />;
+  }
+
   return (
     <Box sx={{ mt: 2, display: 'flex', flexWrap: 'wrap', gap: 1 }}>
-      {[1, 2, 3, 4, 5].map(() => (
+      {data?.tickets.map((ticket) => (
         <TicketCard
-          ticket={{ code: 1234, createdAt: new Date() }}
+          ticket={{ code: ticket.cutNumber, createdAt: new Date() }}
           areas={['corte', 'manga', 'plancha', 'empaque']}
         />
       ))}
