07627721c98cdff46d0704aad8175de872f407ce
Alex Ruiz <ralexs.acu@gmail.com>
2025-12-25 17:44:26 -0600

    feat(Header): Add ticket modal and trigger logic to the "agregar" button

--- src/modules/tickets/components/Header.tsx
+++ src/modules/tickets/components/Header.tsx
@@ -1,13 +1,19 @@
-import { Box, Typography, Button } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
+import { Box, Typography, Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import AddTicketModal from "./AddTicketModal";
+import { useState } from "react";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'space-between', my: 2 }}>
+    <Box sx={{ display: "flex", justifyContent: "space-between", my: 2 }}>
+      <AddTicketModal open={open} setOpen={setOpen} />
+
       <Typography component="span" variant="h6">
         Tickets Recientes
       </Typography>
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
         <Typography component="span" sx={{ mr: 2 }}>
           9 Tickets
         </Typography>
@@ -16,6 +22,7 @@ const Header = () => {
           color="secondary"
           size="small"
           startIcon={<AddIcon />}
+          onClick={() => setOpen(true)}
         >
           Agregar
         </Button>
